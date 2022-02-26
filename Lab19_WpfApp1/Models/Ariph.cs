40297f3dde1eaf596094a93fe89030fca7c98e2b
vladalexgor@ya.ru <vladalexgor@ya.ru>
2022-02-26 12:37:46 +0300

    Добавьте файлы проекта.

---
+++ Lab19_WpfApp1/Models/Ariph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19_WpfApp1.Models
+{
+    static class Ariph
+    {
+        public static double CircleLength(double R)
+        {
+            return 2 * Math.PI * R;
+        }
+    }
+}
---
+++ Lab19_WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Lab19_WpfApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19_WpfApp1.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Ariph.CircleLength(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
